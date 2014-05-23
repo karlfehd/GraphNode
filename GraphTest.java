fcb1a7bdba856043d3f143637a9a68b9ffa3eb05
Karl Fehd <karl.fehd@inbloom.org>
2014-05-22 22:31:00 -0400

    Extended test to additional node

--- GraphTest.java
+++ GraphTest.java
@@ -1,36 +1,54 @@
-import java.util.ArrayList;
+package karl;
 
+import java.util.ArrayList;
 
 public class GraphTest {
 
 	public static void main(String[] args) {
-		
+
 		GNode A = new GraphNode("A");
 		GNode B = new GraphNode("B");
 		GNode C = new GraphNode("C");
 		GNode D = new GraphNode("D");
 		GNode E = new GraphNode("E");
-		
-		((GraphNode) A).addChild(B);
-		((GraphNode) B).addChild(C);
-		((GraphNode) B).addChild(D);
-		((GraphNode) D).addChild(E);
-		
-		ArrayList graph = new ArrayList();
-		graph = A.walkGraph(A);
-		ArrayList path = new ArrayList();
-		path = A.paths(A);
-		
-		System.out.println("Walk graph from node: " + A.getName());
-		for (int i=0; i< graph.size(); i++)
-			System.out.print(((GNode) graph.get(i)).getName() + " ");
-		
-		System.out.println("\n\nPath list from node: " + A.getName());
-		//for (int j=0; j< path.size(); j++)
-			//for (int k=0; k< path.size(); k++)
-			//System.out.print(((GNode) ( (ArrayList) path.get(j)).get(k)).getName() + " ");
-		for (int j=0; j< path.size(); j++)
-			System.out.print(path.get(j) + " ");
+		GNode F = new GraphNode("F");
+		GNode G = new GraphNode("G");
+		GNode H = new GraphNode("H");
+		GNode I = new GraphNode("I");
+		GNode J = new GraphNode("J");
+
+		A.addChild(B);
+		A.addChild(C);
+		A.addChild(D);
+		B.addChild(E);
+		B.addChild(F);
+		C.addChild(G);
+		C.addChild(H);
+		C.addChild(I);
+		D.addChild(J);
+
+		ArrayList<ArrayList> graphA = new ArrayList<ArrayList>();
+		ArrayList<ArrayList> graphB = new ArrayList<ArrayList>();
+		ArrayList<ArrayList> pathA = new ArrayList<ArrayList>();
+		ArrayList<ArrayList> pathB = new ArrayList<ArrayList>();
+
+		graphA = A.walkGraph(A);
+		graphB = B.walkGraph(B);
+		pathA = A.paths(A);
+		pathB = B.paths(B);
+
+		System.out.println("Walk graph from node: A");
+		for (int i = 0; i < graphA.size(); i++)
+			System.out.print(((GNode) graphA.get(i)).getName() + " "); // Old School Print
+
+		System.out.println("\n\nRaw Print of node: B");
+		System.out.print(graphB);
+
+		System.out.println("\n\nPath list from node: A");
+		System.out.print(pathA);
+
+		System.out.println("\n\nPath list from node: B");
+		System.out.print(pathB);
 
 	}
 
