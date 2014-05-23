24e0482a51fc1c1a17c21da4f75d254f398b79e4
Karl Fehd <karl.fehd@inbloom.org>
2014-05-22 22:30:01 -0400

    Fixed logic for paths() and walkGraph()

--- GraphNode.java
+++ GraphNode.java
@@ -1,19 +1,17 @@
+package karl;
+
 import java.util.ArrayList;
 
-
-public class GraphNode implements GNode{
+public class GraphNode implements GNode {
 
 	private String name;
-	private ArrayList children = new ArrayList();
-	boolean visited;
+	private ArrayList<GNode> children = new ArrayList<GNode>();
 
-	public GraphNode(String n){
-
+	public GraphNode(String n) {
 		this.name = n;
-		this.visited = false;
 	}
 
-	public void addChild(GNode n){
+	public void addChild(GNode n) {
 		children.add(n);
 	}
 
@@ -21,67 +19,69 @@ public class GraphNode implements GNode{
 		return name;
 	}
 
+	public String toString() {
+		return name;
+	}
+
 	public GNode[] getChildren() {
 		GNode[] tempNode = new GNode[children.size()];
-		for (int i=0; i < children.size(); i++)
-			tempNode[i] = (GNode) children.get(i);
+		for (int i = 0; i < children.size(); i++)
+			tempNode[i] = children.get(i);
 
 		return tempNode;
 	}
 
 	public ArrayList walkGraph(GNode node) {
-		
+
 		ArrayList tempList = new ArrayList();
 		ArrayList childList = new ArrayList();
-		GraphNode currentnode = (GraphNode) node;
-		boolean childremains = true;
+		childList.add(node);
+		GraphNode currentnode = null;
 
-		while (childremains){
+		while (!childList.isEmpty()) {
 
-			if (!currentnode.visited){
-				
-				currentnode.visited = true;
-				
-				if (!tempList.contains(currentnode))
-					tempList.add(currentnode);
-				
-				if (currentnode.getChildren().length > 0 ){
-					
-					for (int i=0; i < currentnode.getChildren().length; i++){
-						
-						if (!childList.contains(currentnode.getChildren()[i]))
-							childList.add(currentnode.getChildren()[i]);
-					}
+			currentnode = (GraphNode) childList.remove(0);
+
+			if (!tempList.contains(currentnode))
+				tempList.add(currentnode);
+
+			if (currentnode.getChildren().length > 0) {
+
+				for (int i = 0; i < currentnode.getChildren().length; i++) {
+
+					if (!childList.contains(currentnode.getChildren()[i]))
+						childList.add(currentnode.getChildren()[i]);
 				}
 			}
-			
-			if (childList.isEmpty())
-				childremains = false;
-			else
-				currentnode = (GraphNode) childList.remove(0);
 
 		}
 
 		return tempList;
-	}
 
-	public ArrayList paths(GNode node) {
-		ArrayList tempList = new ArrayList();
+	}// end walkGraph
+
+	public ArrayList<ArrayList<GNode>> paths(GNode node) {
+		ArrayList<ArrayList<GNode>> tempList = new ArrayList<ArrayList<GNode>>();
 		GraphNode currentnode = (GraphNode) node;
-		String currentpath = new String(); 
-				
-		if (currentnode.getChildren().length > 0){
-			for (int x=0; x < currentnode.getChildren().length; x++){
-				currentnode.paths(currentnode.getChildren()[x]);
-			}
-			
-			tempList.add(currentpath) ;
-		}
-		else
-			currentpath += currentnode.getName();
-	
-		
-		return tempList;
-	}
 
-}
+		if (currentnode.getChildren().length > 0) {
+			for (GNode child : currentnode.getChildren()) {
+
+				tempList.addAll(paths(child));
+
+			}
+			for (ArrayList<GNode> child : tempList) {
+				child.add(0, currentnode);
+			}
+
+		} else {
+			ArrayList<GNode> thisnode = new ArrayList<GNode>();
+			thisnode.add(currentnode);
+			tempList.add(thisnode);
+		}
+
+		return tempList;
+
+	}// end paths
+
+}// end GraphNode
