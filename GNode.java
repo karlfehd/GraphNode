58e22385a2be2777ee4a33fdd66d32bb211bdc57
Karl Fehd <karl.fehd@inbloom.org>
2014-05-22 22:27:55 -0400

    Update GNode.java

--- GNode.java
+++ GNode.java
@@ -1,9 +1,15 @@
-import java.util.ArrayList;
+package karl;
 
+import java.util.ArrayList;
 
 public interface GNode {
 	public String getName();
-    public GNode[] getChildren();
-    public ArrayList walkGraph(GNode node);
-    public ArrayList paths(GNode node);
+
+	public GNode[] getChildren();
+
+	public void addChild(GNode n);
+
+	public ArrayList walkGraph(GNode node);
+
+	public ArrayList paths(GNode node);
 }
